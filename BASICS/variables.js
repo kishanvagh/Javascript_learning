de8e97c4792d19ae04228859c1f127fbf1fc4d52
kishanvagh <kishanvaghasiya042@gmail.com>
2026-01-01 15:52:31 +0000

    variables

---
+++ BASICS/variables.js
@@ -0,0 +1,11 @@
+let a=5;
+// let a=10; //error   we can't  redeclare let variable
+a=10   //reassign value can possible
+
+var b=10;
+var b=5;  //no error we can redeclare var variables
+
+const c=10;
+// c=5 //error- reassign value in const variable is not possible
+console.table([b,a,c])
+

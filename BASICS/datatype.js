80ab57e46dac1e447e02576b7d8734235360d299
kishanvagh <kishanvaghasiya042@gmail.com>
2026-01-01 15:56:47 +0000

    datatypes

---
+++ BASICS/datatype.js
@@ -0,0 +1,8 @@
+let name="kishan" //string
+let age=19  //numeber
+let isPlace=false  //boolean
+let company;  //datatype of undefied variable is undefined
+let gf=null   //datatype of null is object
+
+console.table([name,age,isPlace,company,gf])
+console.table([typeof name,typeof age,typeof isPlace,typeof company,typeof gf])
